8855292d1bc79862200c2bfb5a10772e79b535f3
ftec16ateneabog <ftec16atenea_bog@unal.edu.co>
2023-11-20 19:17:08 -0500

    add viewModel usuario

--- app/src/main/java/com/atenea/firstviewmodel/MainActivity.java
+++ app/src/main/java/com/atenea/firstviewmodel/MainActivity.java
@@ -1,14 +1,28 @@
 package com.atenea.firstviewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MyViewModel myViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        myViewModel.setNombre("Juan");
+        myViewModel.setEdad(25);
+
+        TextView textView = (TextView) findViewById(R.id.mYTextView);
+        textView.setText("Nombre " + myViewModel.getNombre()+ " Edad: "+ myViewModel.getEdad());
     }
 }
+
+
+
+
---
+++ app/src/main/java/com/atenea/firstviewmodel/MyViewModel.java
@@ -0,0 +1,24 @@
+package com.atenea.firstviewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+    private String nombre;
+    private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
